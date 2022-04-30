004f229cbd5978a6373e6007c5e177e82eb25521
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-04-30 16:49:14 -0400

    created button click script

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
-console.log("Script connected")
+const greenButton = document.querySelector('#green_button');
+const redButton = document.querySelector('#red_button');
+const yellowButton = document.querySelector('#yellow_button');
+const sunCircle = document.querySelectorAll('.sun-circle');
+
+greenButton.addEventListener('click', () => {
+    greenSun()
+})
+redButton.addEventListener('click', () => {
+    redSun()
+})
+yellowButton.addEventListener('click', () => {
+    yellowSun()
+})
+
+function greenSun(){
+ document.getElementById('sun_circle').style.background = "green";
+}
+function redSun(){
+ document.getElementById('sun_circle').style.background = "red";
+}
+function yellowSun(){
+ document.getElementById('sun_circle').style.background = "yellow";
+}
