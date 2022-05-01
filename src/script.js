e720133ed9fbdcd4fc2ddaa1e2b9d03a95e07037
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-04-30 22:41:32 -0400

    added sesame characters

--- src/script.js
+++ src/script.js
@@ -1,24 +1,30 @@
-const greenButton = document.querySelector('#green_button');
-const redButton = document.querySelector('#red_button');
-const yellowButton = document.querySelector('#yellow_button');
-const sunCircle = document.querySelectorAll('.sun-circle');
+const blueButton = document.querySelector("#blue_button");
+const redButton = document.querySelector("#red_button");
+const yellowButton = document.querySelector("#yellow_button");
+const sunCircle = document.querySelectorAll(".sun-circle");
 
-greenButton.addEventListener('click', () => {
-    greenSun()
-})
-redButton.addEventListener('click', () => {
-    redSun()
-})
-yellowButton.addEventListener('click', () => {
-    yellowSun()
-})
+blueButton.addEventListener("click", () => {
+  blueSun();
+});
+redButton.addEventListener("click", () => {
+  redSun();
+});
+yellowButton.addEventListener("click", () => {
+  yellowSun();
+});
 
-function greenSun(){
- document.getElementById('sun_circle').style.background = "green";
+function blueSun() {
+  document.getElementById("sun_circle").style.background = "blue";
+  document.getElementById("character").innerHTML =
+    '<img src="../public/cookie-monster.png" class="cookie-monster" id="cookie_monster" alt="cookie monster character">';
 }
-function redSun(){
- document.getElementById('sun_circle').style.background = "red";
+function redSun() {
+  document.getElementById("sun_circle").style.background = "red";
+  document.getElementById("character").innerHTML =
+    '<img src="../public/elmo.png" class="elmo" id="elmo" alt="elmo character">';
 }
-function yellowSun(){
- document.getElementById('sun_circle').style.background = "yellow";
+function yellowSun() {
+  document.getElementById("sun_circle").style.background = "yellow";
+  document.getElementById("character").innerHTML =
+    '<img src="../public/big-bird.png" class="big-bird" id="big_bird" alt="big bird character">';
 }
