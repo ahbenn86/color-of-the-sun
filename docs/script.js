658bef3c383a303e097d8353f6d755f6840eb0c6
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-10-03 20:41:21 -0400

    folder restructure

--- docs/script.js
+++ docs/script.js
@@ -17,27 +17,27 @@ function blueSun() {
   document.getElementById("thats-correct").innerHTML = "";
   document.getElementById("sun_circle").style.background = "";
   document.getElementById("character").innerHTML =
-    '<img src="../public/cookie-monster.png" class="cookie-monster" id="cookie_monster" alt="cookie monster character">';
+    '<img src="./public/cookie-monster.png" class="cookie-monster" id="cookie_monster" alt="cookie monster character">';
   document.getElementById("chat_bubble").innerHTML =
-    '<img src="../public/blue-bubble.png" id="blue_bubble">';
+    '<img src="./public/blue-bubble.png" id="blue_bubble">';
   document.getElementById("street_sign").style.opacity = "0";
 }
 function redSun() {
   document.getElementById("thats-correct").innerHTML = "";
   document.getElementById("sun_circle").style.background = "";
   document.getElementById("character").innerHTML =
-    '<img src="../public/elmo.png" class="elmo" id="elmo" alt="elmo character">';
+    '<img src="./public/elmo.png" class="elmo" id="elmo" alt="elmo character">';
   document.getElementById("chat_bubble").innerHTML =
-    '<img src="../public/red-bubble.png" id="red_bubble">';
+    '<img src="./public/red-bubble.png" id="red_bubble">';
   document.getElementById("street_sign").style.opacity = "0";
 }
 function yellowSun() {
   document.getElementById("sun_circle").style.background = "yellow";
   document.getElementById("character").innerHTML =
-    '<img src="../public/big-bird.png" class="big-bird" id="big_bird" alt="big bird character">';
+    '<img src="./public/big-bird.png" class="big-bird" id="big_bird" alt="big bird character">';
   document.getElementById("thats-correct").innerHTML =
-    '<img src="../public/thats-correct.png" alt="#">';
+    '<img src="./public/thats-correct.png" alt="#">';
   document.getElementById("chat_bubble").innerHTML =
-    '<img src="../public/yellow-bubble.png" id="yellow_bubble">';
+    '<img src="./public/yellow-bubble.png" id="yellow_bubble">';
   document.getElementById("street_sign").style.opacity = "0";
 }
